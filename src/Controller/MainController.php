ec918935d620ec24815072aa232820317b10f7c0
destiny159 <destiny15988@gmail.com>
2020-11-12 15:25:14 +0200

    created main page design and implemented car color change

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,8 +13,14 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("/cardetails", name="cardetails")
+     */
+    public function carDetais(): Response
+    {
+        return $this->render('main/index2.html.twig');
     }
 }
